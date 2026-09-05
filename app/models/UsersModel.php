a3560908abdacfab0ccdae3d32fca54410901f4e
bacay-roshiel0206 <bacay.roshelp@minsu.edu.ph>
2026-09-05 21:38:25 +0800

    Add get_all_users method to UsersModel

--- app/models/UsersModel.php
+++ app/models/UsersModel.php
@@ -4,8 +4,10 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 class UsersModel extends Model {
 
     protected $table = 'users';
-
     protected $primary_key = 'id';
-  
+
+    public function get_all_users() {
+        return $this->db->table($this->table)->get_all();
+    }
 }
 ?>
