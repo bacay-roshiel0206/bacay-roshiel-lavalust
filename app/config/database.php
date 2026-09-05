61a9ccaa53f26257b5649a047025862a745dcdd8
bacay-roshiel0206 <bacay.roshelp@minsu.edu.ph>
2026-09-05 22:40:20 +0800

    Clean database config to use env vars only

--- app/config/database.php
+++ app/config/database.php
@@ -3,14 +3,14 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'   => 'mysql',
-    'hostname' => getenv('DB_HOST') ?: '',
-    'port'     => getenv('DB_PORT') ?: '',
-    'username' => getenv('DB_USER') ?: '',
-    'password' => getenv('DB_PASSWORD') ?: '',
-    'database' => getenv('DB_NAME') ?: '',
+    'hostname' => getenv('DB_HOST') ?: $_ENV['DB_HOST'],
+    'port'     => getenv('DB_PORT') ?: $_ENV['DB_PORT'],
+    'username' => getenv('DB_USER') ?: $_ENV['DB_USER'],
+    'password' => getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'],
+    'database' => getenv('DB_NAME') ?: $_ENV['DB_NAME'],
     'dbprefix' => '',
     'charset'  => 'utf8mb4',
     'path'     => ''
 );
 
-?>
+return $database;
